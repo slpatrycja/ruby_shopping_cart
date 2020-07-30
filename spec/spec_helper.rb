3510776fc14ef71919b2de701e51aa6d4a8e04f3
Patrycja Słabosz <sl.patrycja.of@gmail.com>
2020-07-30 17:42:30 +0200

    Add spec_helper and .rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'pry'
+
+require_relative '../lib/promotional_rules/total_discount.rb'
+require_relative '../lib/promotional_rules/items_batch_discount.rb'
+require_relative '../lib/item.rb'
+require_relative '../lib/basket.rb'
+require_relative '../lib/checkout.rb'
